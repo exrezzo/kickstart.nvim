bb100b709eb6a30b37a8b2856121f5950c0c5183
Ivan Ardillo <ivan.ardillo@gmail.com>
2025-02-08 23:38:41 +0100

    jetbrains-mono-nerd-font usage memo

--- init.lua
+++ init.lua
@@ -917,6 +917,9 @@ require('lazy').setup({
     'folke/tokyonight.nvim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     init = function()
+      -- Ho installato in iTerm2 il font (magnifico)
+      -- con brew install `font-jetbrains-mono-nerd-font` e uso ExtraBold
+
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
