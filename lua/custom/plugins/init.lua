a2a6010276e3222c65d3679b6987485fb18cb7e6
Ivan Ardillo <ivan.ardillo@gmail.com>
2025-02-10 10:30:39 +0100

    use forked copilot chat + markdown previewer

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -51,7 +51,16 @@ return {
     end,
   },
   {
-    'CopilotC-Nvim/CopilotChat.nvim',
+    'iamcco/markdown-preview.nvim',
+    cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
+    build = 'cd app && yarn install',
+    init = function()
+      vim.g.mkdp_filetypes = { 'markdown' }
+    end,
+    ft = { 'markdown' },
+  },
+  {
+    'exrezzo/CopilotChat.nvim',
     dependencies = {
       { 'github/copilot.vim' }, -- or zbirenbaum/copilot.lua
       { 'nvim-lua/plenary.nvim', branch = 'master' }, -- for curl, log and async functions
@@ -61,68 +70,6 @@ return {
       require('CopilotChat').setup {
         -- See Configuration section for options
         model = 'claude-3.5-sonnet',
-        -- ho modificato il sorgente del plugin aggiungendo una funzione per il context
-        -- scopiazzando e modificando la funzione M.files(winnr, with_content):
-        --
-        -- function M.filesHidden(winnr, with_content)
-        --   local cwd = utils.win_cwd(winnr)
-        --
-        --   notify.publish(notify.STATUS, 'Scanning files')
-        --
-        --   local files = utils.scan_dir(cwd, {
-        --     hidden = true,
-        --     add_dirs = false,
-        --     respect_gitignore = true,
-        --   })
-        --
-        --   notify.publish(notify.STATUS, 'Reading files')
-        --
-        --   local out = {}
-        --
-        --   -- Create file list in chunks
-        --   local chunk_size = 100
-        --   for i = 1, #files, chunk_size do
-        --     local chunk = {}
-        --     for j = i, math.min(i + chunk_size - 1, #files) do
-        --       table.insert(chunk, files[j])
-        --     end
-        --
-        --     local chunk_number = math.floor(i / chunk_size)
-        --     local chunk_name = chunk_number == 0 and 'file_map' or 'file_map' .. tostring(chunk_number)
-        --
-        --     table.insert(out, {
-        --       content = table.concat(chunk, '\n'),
-        --       filename = chunk_name,
-        --       filetype = 'text',
-        --     })
-        --   end
-        --
-        --   -- Read all files if we want content as well
-        --   if with_content then
-        --     async.util.scheduler()
-        --
-        --     files = vim.tbl_filter(
-        --       function(file)
-        --         return file.ft ~= nil
-        --       end,
-        --       vim.tbl_map(function(file)
-        --         return {
-        --           name = utils.filepath(file),
-        --           ft = utils.filetype(file),
-        --         }
-        --       end, files)
-        --     )
-        --
-        --     for _, file in ipairs(files) do
-        --       local file_data = get_file(file.name, file.ft)
-        --       if file_data then
-        --         table.insert(out, file_data)
-        --       end
-        --     end
-        --   end
-        --
-        --   return out
-        -- end
         contexts = {
           hiddenFiles = {
             resolve = function(input, source)
