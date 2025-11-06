4ed246b02abc182d852b1db1a10cdc4528767d36
Ivan Ardillo <ivan.ardillo@gmail.com>
2025-11-06 14:56:48 +0100

    fix completeopt var

--- init.lua
+++ init.lua
@@ -111,7 +111,7 @@ vim.o.mouse = 'a'
 
 -- Don't show the mode, since it's already in the status line
 vim.o.showmode = false
-vim.o.completeopt = { 'menu', 'menuone', 'noselect', 'popup' }
+vim.opt.completeopt = { 'menu', 'menuone', 'noselect', 'popup' }
 
 -- Sync clipboard between OS and Neovim.
 --  Schedule the setting after `UiEnter` because it can increase startup-time.
