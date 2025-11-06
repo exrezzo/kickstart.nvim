826fa8a4431d1fc103a4f7ff39e2452182aa5b5f
Ivan Ardillo <ivan.ardillo@gmail.com>
2025-11-06 13:53:28 +0100

    use original copilot plugin again

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -87,7 +87,7 @@ return {
     },
   },
   {
-    'exrezzo/CopilotChat.nvim',
+    'CopilotC-Nvim/CopilotChat.nvim',
     dependencies = {
       { 'github/copilot.vim' }, -- or zbirenbaum/copilot.lua
       { 'nvim-lua/plenary.nvim', branch = 'master' }, -- for curl, log and async functions
